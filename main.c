843ce1ad6ceeadededb9dc7693eb150b15cf64dc
Ddystopia <sashkamaman@gmail.com>
2022-02-16 16:01:09 +0200

    improvements

--- main.c
+++ main.c
@@ -3,37 +3,36 @@
 #include <stdlib.h>
 
 extern double sin_c(double);
-
-
-const double EPSILON = 1e-15;
 void test();
 
-int main(int argc, char** argv) {
+const double EPSILON = 1e-14;
+
+int main(int argc, char **argv) {
   if (argc < 2)
     test();
   else
     printf("Tesed:\t%.20lf\nC:\t%.20lf\n", sin_c(atof(argv[1])),
-        sin(atof(argv[1])));
+           sin(atof(argv[1])));
 
   return 0;
 }
 
 void test() {
-  printf("Testing segment [-10; 10].\n");
+  printf("Testing segment [-10; 10).\n");
   printf("Epsilon: %le\n", EPSILON);
   // I'm counting on margin of error
-  for (double i = -10; i <= 10; i += 0.01) {
+  for (double i = -10; i < 10; i += 0.00001) {
     double c = sin(i);
     double tested = sin_c(i);
     double diff = fabs(tested - c);
     if (diff > EPSILON) {
       printf("\n\033[1m\033[31mFailed\033[0m\n");
       printf(
-          "N:\t%.16lf\
-          \nDiff:\t%le\
-          \nTested:\t%.29lf\
-          \nC:\t%.29lf\n",
-          i, diff, tested, c);
+         "N:\t%.29lf        \
+         \nDiff:\t%.29lf    \
+         \nTested:\t%.29lf  \
+         \nC:\t%.29lf\n",
+             i, diff, tested, c);
       exit(1);
     }
   }
--- sin.s
+++ sin.s
@@ -9,12 +9,10 @@ sin_c:
   mulsd TAU(%rip), %xmm1
   subsd %xmm1, %xmm0
   movapd %xmm0, %xmm2
-  // %xmm0 - x %xmm2 - xx %xmm1 - res
   mulsd %xmm0, %xmm2
-  movsd 13*8+fci(%rip), %xmm1
+  // %xmm0 = x %xmm2 = xx %xmm1 = res
+  movsd 12*8+fci(%rip), %xmm1
   // cycle begin
-  mulsd %xmm2, %xmm1
-  addsd 12*8+fci(%rip), %xmm1
   mulsd %xmm2, %xmm1
   addsd 11*8+fci(%rip), %xmm1
   mulsd %xmm2, %xmm1
@@ -38,7 +36,7 @@ sin_c:
   mulsd %xmm2, %xmm1
   addsd 1*8+fci(%rip), %xmm1
   mulsd %xmm2, %xmm1
-  addsd fci(%rip), %xmm1
+  addsd 0*8+fci(%rip), %xmm1
   // cycle end
   mulsd %xmm1, %xmm0
   ret
@@ -48,8 +46,8 @@ fci: // (-1)^i * 1/(2i+1)!, i is index
   .double  1
   .double -0.166666666666666666666666666666666666
   .double  0.008333333333333333333333333333333333
-  .double -0.000198412698412698412526317115478491
-  .double  0.000002755731922398589251095059327045
+  .double -0.000198412698412698412698412698412698
+  .double  0.000002755731922398589065227557319223
   .double -0.000000025052108385441720223866179321
   .double  0.000000000160590438368216159258023325
   .double -0.000000000000764716373181981741525580
@@ -58,13 +56,7 @@ fci: // (-1)^i * 1/(2i+1)!, i is index
   .double  0.000000000000000000019572941063391262
   .double -0.000000000000000000000038681701706306
   .double  0.000000000000000000000000064469502843
-  .double -0.000000000000000000000000000091836898
 RTAU: // 1 / 2pi
   .double .159154943091895345608222100963757839
 TAU: // 2pi
   .double 6.283185307179586231995926937088370323
-PI:
-  .double 3.141592653589793238462643383279502884
-HALF:
-  .double 0.5
-
